4b34cb8eb974fcbde5e604f4a8b7f4c9f8deac1e
Hussein Mahmoud <husseinalobaidi77@gmail.com>
2018-04-25 10:10:04 +0300

    Add Folder DataAccess

---
+++ searchproperty/DataAccess/ADO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace searchproperty.DataAccess
+{
+    public class ADO
+    {
+        public int InsertMainDetails(string BookNo, string BookDate, string HereInsightOffice, string InformedOffice)
+        {
+            Int32 id;
+            string sql= "sp_InsertMainDetails";
+            using (SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings[""].ConnectionString))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BookNo", SqlDbType.NVarChar, 50).Value = BookNo;
+                    if (!string.IsNullOrEmpty(BookDate)) { cmd.Parameters.Add("@BookDate", SqlDbType.DateTime).Value = Convert.ToDateTime(BookDate); }
+                    cmd.Parameters.Add("@HereInsightOffice", SqlDbType.NVarChar, 250).Value = HereInsightOffice;
+                    cmd.Parameters.Add("@InformedOffice", SqlDbType.NVarChar, 250).Value = InformedOffice;
+                    id = Convert.ToInt32( cmd.ExecuteScalar());
+                }
+            }
+            return id;
+        }
+    }
+}
